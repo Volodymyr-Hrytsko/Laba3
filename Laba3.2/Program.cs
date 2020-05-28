9f5408892a5d118dfd02e187434d519be66194e5
Volodymyr-Hrytsko <grickovova18@gmail.com>
2020-05-28 21:09:47 +0300

    labka3

---
+++ Laba3.2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Text;
+namespace laba3._2
+{
+    class Test
+    {
+        public static void Main()
+        {
+            string pathin = @"C:\Users\gric\source\repos\Laba3.2\bin\Debug\in.txt";
+
+            StreamReader f = new StreamReader(pathin);
+            string v = f.ReadLine();
+
+            string path = @"C:\Users\gric\source\repos\Laba3.2\bin\Debug\out.txt";
+
+            // This text is added only once to the file.
+            if (!File.Exists(path))
+            {
+                // Create a file to write to.
+                string createText = "Hello and Welcome" + Environment.NewLine;
+                File.WriteAllText(path, createText);
+            }
+
+            string text = "";
+            string[] arrStr = new string[2];
+            string[] split = v.Split(new Char[] { '!' });                                  //БУЛО
+            int i = 0;
+            foreach (string s in split)
+            {
+                if (s.Trim() != "")
+                {
+
+                    arrStr[i] = s;
+                    i++;
+                    text = s;                                                              //СТАЛО
+                    break;
+                }
+            }
+            string str = "";
+            try
+            {
+                using (StreamReader sr = new StreamReader(pathin))
+                {
+                    sr.ReadLine(); 
+                    str = sr.ReadToEnd();
+                }
+                using (StreamWriter sw = new StreamWriter(@"C:\Users\gric\source\repos\Laba3.2\bin\Debug"))
+                {
+                    sw.WriteLine(str); 
+                }
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine("Error {0}", ex.Message);
+            }
+            Console.WriteLine(str);
+            Console.ReadKey();
+            File.AppendAllText(path, text);
+            string readText = File.ReadAllText(path);
+            Console.WriteLine(readText);
+        }
+    }
+}
---
+++ Laba3.3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Linq;
+namespace laba3.3
+{
+    class WordCounter
+    {
+        static void Main()
+        {
+            String line;
+            int count = 0;
+            string counts = "";
+
+            //Opens a file in read mode  
+            System.IO.StreamReader file = new System.IO.StreamReader(@"C:\Users\gric\source\repos\Laba3.3\bin\Debug\input.txt");
+
+            //Gets each line till end of file is reached  
+            while ((line = file.ReadLine()) != null)
+            {
+                //Splits each line into words  
+                String[] words = line.Split(' ');
+                //Counts each word  
+                count = count + words.Length;
+            }
+            counts += count;
+            Console.WriteLine("Number of words present in given file: " + counts);
+            string path = @"C:\Users\gric\source\repos\Laba3.3\bin\Debug\output.txt";
+            File.AppendAllText(path, counts);
+            string readText = File.ReadAllText(path);
+            Console.WriteLine(readText);
+        }
+    }
+}
